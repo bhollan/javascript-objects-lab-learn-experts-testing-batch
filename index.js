38e90a3308a3f9ff9cfe5169c0482391c39520e9
Brian Holland <beehollander@gmail.com>
2017-01-18 18:28:35 -0600

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+const recipes = {}
+
+function updateObjectWithKeyAndValue(obj, key, val){
+  return Object.assign(obj, {[key]:val});
+};
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, val){
+  obj[key] = val;
+  return obj;
+};
+
+function deleteFromObjectByKey(obj, key, val){
+  var copy = Object.assign({}, obj);
+  delete copy[key];
+  return copy;
+};
+
+function destructivelyDeleteFromObjectByKey(obj, key){
+  delete obj.key;
+  return obj;
+};
